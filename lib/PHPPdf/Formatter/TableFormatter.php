8d86b80bca01e5b29e7bfe6be9194339aba26116
psliwa <peter.pl7@gmail.com>
2011-03-26 22:26:35 +0100

    table formatting

--- lib/PHPPdf/Formatter/TableFormatter.php
+++ lib/PHPPdf/Formatter/TableFormatter.php
@@ -11,81 +11,27 @@ use PHPPdf\Glyph\Glyph,
  */
 class TableFormatter extends BaseFormatter
 {
-    //TODO refactoring
     public function format(Glyph $glyph, Document $document)
     {
-        $rows = $glyph->getChildren();
+        $columnsWidths = $glyph->getWidthsOfColumns();
 
-        $cellMaxWidths = array();
-        $cellMaxWidthsWidthMargins = array();
-        foreach($rows as $row)
+        foreach($glyph->getChildren() as $row)
         {
-            $cells = $row->getChildren();
-
-            foreach($cells as $index => $cell)
-            {
-                $width = $cell->getWidth();
-                $widthWidthMargin = $width + $cell->getMarginLeft() + $cell->getMarginRight();
-
-                if(!isset($cellMaxWidths[$index]) || $cellMaxWidths[$index] < $width)
-                {
-                    $cellMaxWidths[$index] = $width;
-                }
-
-                if(!isset($cellMaxWidthsWidthMargins[$index]) || $cellMaxWidthsWidthMargins[$index] < $widthWidthMargin)
-                {
-                    $cellMaxWidthsWidthMargins[$index] = $widthWidthMargin;
-                }
-            }
-        }
-
-        $widthOfAllCells = array_sum($cellMaxWidths);
-        $diff = $glyph->getWidth() - $widthOfAllCells;
-
-        $widthWithMarginOfAllCells = array_sum($cellMaxWidthsWidthMargins);
-        $diffWithMargin = $glyph->getWidth() - $widthWithMarginOfAllCells;
-
-        if($diffWithMargin > 0)
-        {
-            $cellsExpand = $diff/count($cellMaxWidths);
-
-            array_walk($cellMaxWidths, function(&$value, $key) use ($cellsExpand)
-            {
-                $value += $cellsExpand;
-            });
-        }
-        else
-        {
-            $glyph->setWidth($glyph->getWidth()-$diffWithMargin);
-            $boundary = $glyph->getBoundary();
-            $boundary->pointTranslate(1, -$diffWithMargin, 0);
-            $boundary->pointTranslate(2, -$diffWithMargin, 0);
-
-            foreach($glyph->getChildren() as $row)
-            {
-                $boundary = $row->getBoundary();
-                $boundary->pointTranslate(1, -$diffWithMargin, 0);
-                $boundary->pointTranslate(2, -$diffWithMargin, 0);
-            }
-        }
-
-        foreach($rows as $row)
-        {
-            $cells = $row->getChildren();
             $translate = 0;
-            foreach($cells as $index => $cell)
+            foreach($row->getChildren() as $column => /* @var $cell PHPPdf\Glyph\Table\Cell */ $cell)
             {
-                $diffInHeight = $row->getHeight() - ($cell->getHeight() + $cell->getMarginTop() + $cell->getMarginBottom());
-                $cellExpand = $cellMaxWidths[$index] - $cell->getWidth();
-                $cell->translate($translate, 0);
-                $boundary = $cell->getBoundary();
-                $boundary->pointTranslate(1, $cellExpand, 0);
-                $boundary->pointTranslate(2, $cellExpand, $diffInHeight);
-                $boundary->pointTranslate(3, 0, $diffInHeight);
-                $cell->setWidth($cell->getWidth() + $cellExpand);
-                $cell->setHeight($cell->getHeight() + $diffInHeight);
+                $newWidth = $columnsWidths[$column];
+                $currentWidth = $cell->getWidth();
+                $diff = $newWidth - $currentWidth;
 
-                $translate += $cellExpand;
+                $cell->setWidth($newWidth);
+                $cell->translate($translate, 0);
+
+                $boundary = $cell->getBoundary();
+                $boundary->pointTranslate(1, $diff, 0)
+                         ->pointTranslate(2, $diff, 0);
+
+                $translate += $newWidth;
             }
         }
     }
--- tests/Formatter/TableFormatterTest.php
+++ tests/Formatter/TableFormatterTest.php
@@ -15,77 +15,81 @@ class TableFormatterTest extends PHPUnit_Framework_TestCase
 
     /**
      * @test
+     * @dataProvider cellsWidthProvider
      */
-    public function steadyCellWidth()
+    public function equalizeCells(array $cellsWidthInRows, array $columnsWidths)
     {
-        $table = $this->getMock('PHPPdf\Glyph\Table', array('getWidth', 'getChildren'));
-        $table->expects($this->exactly(2))
-              ->method('getWidth')
-              ->will($this->returnValue(400));
-
-        $row = $this->getMock('PHPPdf\Glyph\Table\Row', array('getParent', 'getChildren'));
-        $row->expects($this->atLeastOnce())
-            ->method('getParent')
-            ->will($this->returnValue($table));
-
-        $width = 25;
-        for($i=0; $i<3; $i++)
+        $rows = array();
+        foreach($cellsWidthInRows as $widths)
         {
-            $cells[] = $this->getCellMock(0, 300, 0, 20, $width);
+            $translate = 0;
+            $cells = array();
+            foreach($widths as $column => $width)
+            {
+                $columnWidth = $columnsWidths[$column];
+                $cells[] = $this->getCellMockWithTranslateAndResizeExpectations($width, $columnWidth, $translate);
+                $translate += $columnWidth;
+            }
+
+            $row = $this->getMock('PHPPdf\Glyph\Table\Row', array('getChildren'));
+            $row->expects($this->atLeastOnce())
+                ->method('getChildren')
+                ->will($this->returnValue($cells));
+            $rows[] = $row;
         }
 
-        $table->expects($this->once())
+        $table = $this->getMock('PHPPdf\Glyph\Table', array('getChildren', 'getWidthsOfColumns'));
+        $table->expects($this->atLeastOnce())
               ->method('getChildren')
-              ->will($this->returnValue(array($row)));
-
-        $cellWithWidth = $this->getCellMock(0, 300, 300, 20, 325);
-        $cells[] = $cellWithWidth;
-
-        $row->expects($this->exactly(2))
-            ->method('getChildren')
-            ->will($this->returnValue($cells));
+              ->will($this->returnValue($rows));
+        $table->expects($this->atLeastOnce())
+              ->method('getWidthsOfColumns')
+              ->will($this->returnValue($columnsWidths));
 
         $this->formatter->format($table, new Document());
-
-        $prevCellBoundary = null;
-        foreach($cells as $cell)
-        {
-            $boundary = $cell->getBoundary();
-            if($prevCellBoundary)
-            {
-                $this->assertEquals($prevCellBoundary[1], $boundary[0]);
-            }
-            $prevCellBoundary = $boundary;
-        }
     }
 
-    private function getCellMock($x, $y, $width, $height, $setWidthConstraint)
+    private function getCellMockWithTranslateAndResizeExpectations($width, $newWidth, $translateX)
     {
-        $cell = $this->getMock('PHPPdf\Glyph\Table\Cell', array('setWidth', 'getWidth', 'getBoundary', 'setHeight', 'getHeight'));
-
-        $cell->expects($this->once())
-             ->method('setWidth')
-             ->with($setWidthConstraint)
-             ->will($this->returnValue($cell));
-
-        $cell->expects($this->any())
-             ->method('getWidth')
-             ->will($this->returnValue($width));
+        $boundary = $this->getMock('PHPPdf\Util\Boundary', array('pointTranslate'));
+        $cell = $this->getMock('PHPPdf\Glyph\Table\Cell', array('getWidth', 'getBoundary', 'setWidth', 'translate'));
 
         $cell->expects($this->atLeastOnce())
-             ->method('getHeight')
-             ->will($this->returnValue($height));
-
-        $boundary = new Boundary();
-        $boundary->setNext($x, $y)
-                 ->setNext($x+$width, $y)
-                 ->setNext($x+$width, $y-$height)
-                 ->setNext($x, $y-$height)
-                 ->close();
-        $cell->expects($this->any())
+             ->method('getWidth')
+             ->will($this->returnValue($width));
+        $cell->expects($this->once())
+             ->method('setWidth')
+             ->with($newWidth);
+        $cell->expects($this->atLeastOnce())
              ->method('getBoundary')
              ->will($this->returnValue($boundary));
 
+        $cell->expects($this->once())
+             ->method('translate')
+             ->with($translateX, 0);
+        $diff = $newWidth - $width;
+        $boundary->expects($this->at(0))
+                 ->method('pointTranslate')
+                 ->with(1, $diff, 0)
+                 ->will($this->returnValue($boundary));
+        $boundary->expects($this->at(1))
+                 ->method('pointTranslate')
+                 ->with(2, $diff, 0)
+                 ->will($this->returnValue($boundary));
+
         return $cell;
+    }
+
+    public function cellsWidthProvider()
+    {
+        return array(
+            array(
+                array(
+                    array(10, 20, 30),
+                    array(40, 10, 15),
+                ),
+                array(50, 20, 30),
+            ),
+        );
     }
 }
