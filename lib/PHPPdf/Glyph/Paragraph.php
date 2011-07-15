311c58f829a5fc1adf193283f8f3521e3f652c03
psliwa <peter.pl7@gmail.com>
2011-07-15 22:21:23 +0200

    add paragraph tag to config files

--- lib/PHPPdf/Glyph/Paragraph.php
+++ lib/PHPPdf/Glyph/Paragraph.php
@@ -1,8 +1,20 @@
 <?php
 
+/*
+ * Copyright 2011 Piotr Śliwa <peter.pl7@gmail.com>
+ *
+ * License information is in LICENSE file
+ */
+
 namespace PHPPdf\Glyph;
 
-
+/**
+ * @author Piotr Śliwa <peter.pl7@gmail.com>
+ */
 class Paragraph extends Container
 {
+    public function getAttribute($name)
+    {
+        return $this->getParent()->getAttribute($name);
+    }
 }
