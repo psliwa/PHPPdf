5fa25b1b1708f32c75510a318d4d82b095032e78
psliwa <peter.pl7@gmail.com>
2011-09-07 21:06:36 +0200

    test bootstrap.php fix

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,9 +2,7 @@
 
 define('TEST_RESOURCES_DIR', __DIR__.'/PHPPdf/Resources');
 
-set_include_path(dirname(__FILE__).'/../lib' . PATH_SEPARATOR. dirname(__FILE__).'/../lib/vendor' . PATH_SEPARATOR . dirname(__FILE__).'/../lib/vendor/Zend' . PATH_SEPARATOR . get_include_path());
-
-require_once 'PHPPdf/Autoloader.php';
+require_once __DIR__.'/../lib/PHPPdf/Autoloader.php';
 PHPPdf\Autoloader::register();
 PHPPdf\Autoloader::register(__DIR__.'/../lib/vendor');
 PHPPdf\Autoloader::register(__DIR__.'/');
