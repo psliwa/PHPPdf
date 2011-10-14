51bd42460fe9611ea5df41c3a195ee09d9535606
psliwa <peter.pl7@gmail.com>
2011-10-14 22:06:56 +0200

    FacadeAware doc

--- lib/PHPPdf/Parser/FacadeAware.php
+++ lib/PHPPdf/Parser/FacadeAware.php
@@ -1,7 +1,18 @@
 <?php
 
+/*
+ * Copyright 2011 Piotr Śliwa <peter.pl7@gmail.com>
+ *
+ * License information is in LICENSE file
+ */
+
 namespace PHPPdf\Parser;
 
+/**
+ * Interface of type that is aware of Facade existance.
+ *
+ * @author Piotr Śliwa <peter.pl7@gmail.com>
+ */
 interface FacadeAware
 {
     public function setFacade(Facade $facade);
