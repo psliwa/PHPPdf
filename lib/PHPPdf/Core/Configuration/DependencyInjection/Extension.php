8bb26d162fe0a66e4bae4131ab049463fa123e15
psliwa <peter.pl7@gmail.com>
2011-10-19 22:26:47 +0200

    fix Symfony2 configuration

--- lib/PHPPdf/Core/Configuration/DependencyInjection/Extension.php
+++ lib/PHPPdf/Core/Configuration/DependencyInjection/Extension.php
@@ -22,7 +22,7 @@ class Extension implements ExtensionInterface
 {
     public function load(array $config, ContainerBuilder $container)
     {
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../../Resources/config/services'));
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../../../Resources/config/services'));
         $loader->load('nodes.xml');
     }
     
