1381f56e5daa8cda95f161bee246a0522fb3d228
psliwa <peter.pl7@gmail.com>
2011-11-13 20:19:27 +0100

    change default Zend_Pdf dependant repository to psliwa fork.

--- vendors.php
+++ vendors.php
@@ -9,7 +9,7 @@ function fetchGitVendors()
         array('Symfony/Component/DependencyInjection', 'git://github.com/symfony/DependencyInjection.git', 'master'),
         array('Symfony/Component/Config', 'git://github.com/symfony/Config.git', 'master'),
         array('Markdown', 'git://github.com/wolfie/php-markdown.git', 'd464071334'),
-        array('Zend/Pdf', 'git://github.com/knplabs/zend-pdf.git', 'master'),
+        array('Zend/Pdf', 'git://github.com/psliwa/zend-pdf.git', 'master'),
         array('Zend/Memory', 'git://github.com/knplabs/zend-memory.git', 'master'),
         array('Zend/Cache', 'git://github.com/knplabs/zend-cache.git', 'master'),
     );
