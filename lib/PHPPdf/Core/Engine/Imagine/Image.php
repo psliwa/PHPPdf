ae1ff6eb13171c1acad615514db32f00ed140d3f
psliwa <peter.pl7@gmail.com>
2011-12-06 21:47:55 +0100

    Image implementation for Imagine

---
+++ lib/PHPPdf/Core/Engine/Imagine/Image.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+ * Copyright 2011 Piotr Śliwa <peter.pl7@gmail.com>
+ *
+ * License information is in LICENSE file
+ */
+
+
+namespace PHPPdf\Core\Engine\Imagine;
+
+use PHPPdf\Exception\InvalidResourceException;
+use Imagine\Exception\RuntimeException;
+use Imagine\Image\ImagineInterface;
+use PHPPdf\Core\Engine\Image as BaseImage;
+
+/**
+ * Image implementation for Imagine
+ * 
+ * @author Piotr Śliwa <peter.pl7@gmail.com>
+ */
+class Image implements BaseImage
+{
+    private $imagePath;
+    private $imagine;
+    private $image;
+    
+    public function __construct($imagePath, ImagineInterface $imagine)
+    {
+        $this->imagePath = $imagePath;
+        $this->imagine = $imagine;
+    }
+    
+    public function getOriginalWidth()
+    {
+        return $this->getWrappedImage()->getSize()->getWidth();
+    }
+    
+    /**
+     * @internal
+     */
+    public function getWrappedImage()
+    {
+        if($this->image === null)
+        {
+            try
+            {
+                $this->image = $this->imagine->open($this->imagePath);
+            }
+            catch(\Imagine\Exception\Exception $e)
+            {
+                InvalidResourceException::invalidImageException($this->imagePath, $e);
+            }
+        }
+        
+        return $this->image;
+    }
+    
+    public function getOriginalHeight()
+    {
+        return $this->getWrappedImage()->getSize()->getHeight();
+    }
+}
---
+++ tests/PHPPdf/Test/Core/Engine/Imagine/ImageTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace PHPPdf\Test\Core\Engine\Imagine;
+
+use PHPPdf\Core\Engine\Imagine\Image;
+use Imagine\Gd\Imagine;
+use PHPPdf\PHPUnit\Framework\TestCase;
+
+class ImageTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function createImageObject()
+    {
+        $imagine = new Imagine();
+        $image = new Image(TEST_RESOURCES_DIR.'/domek.png', $imagine);
+        
+        $imagineImage = $image->getWrappedImage();
+        
+        $this->assertEquals($imagineImage->getSize()->getHeight(), $image->getOriginalHeight());
+        $this->assertEquals($imagineImage->getSize()->getWidth(), $image->getOriginalWidth());
+    }
+    
+    /**
+     * @test
+     * @expectedException PHPPdf\Exception\InvalidResourceException
+     */
+    public function throwExceptionOnUnexistedImage()
+    {
+        $imagine = new Imagine();
+        $image = new Image('some path', $imagine);
+        
+        $image->getWrappedImage();
+    }
+}
