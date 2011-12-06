fc05415c956035af427d19da92d7d067f76ef9fc
psliwa <peter.pl7@gmail.com>
2011-12-06 22:26:09 +0100

    GraphicsContext for Imagine - prototype implementation

---
+++ lib/PHPPdf/Core/Engine/Imagine/GraphicsContext.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace PHPPdf\Core\Engine\Imagine;
+
+use PHPPdf\Core\Engine\AbstractGraphicsContext;
+use PHPPdf\Core\Engine\GraphicsContext as BaseGraphicsContext;
+use PHPPdf\Core\Engine\Image as BaseImage;
+use PHPPdf\Core\Engine\Font as BaseFont;
+
+class GraphicsContext extends AbstractGraphicsContext
+{
+    private static $originalState = array(
+        'fillColor' => null,
+        'lineColor' => null,
+        'lineWidth' => null,
+        'lineDashingPattern' => null,
+        'alpha' => 1,
+        'font' => null,
+        'fontSize' => null,
+    );
+    
+    private $stateStack = array();
+    
+    private $state = array();
+    
+    public function __construct()
+    {
+        $this->state = self::$originalState;
+    }
+    
+    protected function doClipRectangle($x1, $y1, $x2, $y2)
+    {
+        
+    }
+    
+    protected function doSaveGS()
+    {
+        array_unshift($this->stateStack, $this->state);
+    }
+    
+    protected function doRestoreGS()
+    {
+        $state = array_shift($this->stateStack);
+        
+        if($state === null)
+        {
+            $state = self::$originalState;
+        }
+        
+        $this->state = $state;
+    }
+    
+    protected function doDrawImage(BaseImage $image, $x1, $y1, $x2, $y2)
+    {
+        
+    }
+    
+    protected function doDrawLine($x1, $y1, $x2, $y2)
+    {
+        
+    }
+    
+    protected function doSetFillColor($colorData)
+    {
+        $this->state['fillColor'] = $colorData;
+    }
+    
+    protected function doSetLineColor($colorData)
+    {
+        $this->state['lineColor'] = $colorData;
+    }
+    
+    protected function doDrawPolygon(array $x, array $y, $type)
+    {
+        
+    }
+    
+    protected function doDrawText($text, $x, $y, $encoding, $wordSpacing = 0, $fillType = self::SHAPE_DRAW_FILL)
+    {
+        
+    }
+    
+    protected function doDrawRoundedRectangle($x1, $y1, $x2, $y2, $radius, $fillType = self::SHAPE_DRAW_FILL_AND_STROKE)
+    {
+        
+    }
+    
+    protected function doSetLineWidth($width)
+    {
+        $this->state['lineWidth'] = $width;
+    }
+    
+    protected function doSetLineDashingPattern($pattern)
+    {
+        $this->state['lineDashingPattern'] = $pattern;
+    }
+    
+    protected function doUriAction($x1, $y1, $x2, $y2, $uri)
+    {
+        //not supported
+    }
+    
+    protected function doGoToAction(BaseGraphicsContext $gc, $x1, $y1, $x2, $y2, $top)
+    {
+        //not supported
+    }
+    
+    protected function doAttachStickyNote($x1, $y1, $x2, $y2, $text)
+    {
+        //not supported
+    }
+    
+    protected function doSetAlpha($alpha)
+    {
+        $this->state['alpha'] = $alpha;
+    }
+    
+    protected function doRotate($x, $y, $angle)
+    {
+        
+    }
+    
+    public function getWidth()
+    {
+        
+    }
+    
+    public function getHeight()
+    {
+        
+    }
+    
+    public function setFont(BaseFont $font, $size)
+    {
+        $this->addToQueue('doSetFont', func_get_args());
+    }
+    
+    protected function doSetFont(BaseFont $font, $size)
+    {
+        $this->state['font'] = $font;
+        $this->state['fontSize'] = $size;
+    }
+    
+    public function addBookmark($identifier, $name, $top, $ancestorsIdentifier = null)
+    {
+        //not supported
+    }
+    
+    public function copy()
+    {
+        
+    }
+}
---
+++ tests/PHPPdf/Test/Core/Engine/Imagine/GraphicsContextTest.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace PHPPdf\Test\Core\Engine\Imagine;
+
+use PHPPdf\Core\Engine\Imagine\GraphicsContext;
+
+use PHPPdf\PHPUnit\Framework\TestCase;
+
+class GraphicsContextTest extends TestCase
+{
+    private $image;
+    private $gc;
+    
+    public function setUp()
+    {
+        $this->image = $this->getMock('Imagine\Image\ImageInterface');
+        $this->gc = new GraphicsContext($this->image);
+    }
+    
+    /**
+     * @test
+     */
+    public function gsState()
+    {        
+        $this->gc->setFillColor('#111111');
+        $this->gc->saveGS();
+        $this->gc->commit();
+
+        $state = $this->readCurrentGsState();
+        $this->assertEquals('#111111', $state['fillColor']);
+        
+        $this->gc->setFillColor('#222222');
+        $this->gc->saveGS();
+        $this->gc->commit();
+        
+        $state = $this->readCurrentGsState();
+        $this->assertEquals('#222222', $state['fillColor']);
+        
+        $this->gc->restoreGS();
+        $this->gc->commit();
+        
+        $state = $this->readCurrentGsState();
+        $this->assertEquals('#222222', $state['fillColor']);
+        
+        $this->gc->restoreGS();
+        $this->gc->commit();
+        
+        $state = $this->readCurrentGsState();
+        $this->assertEquals('#111111', $state['fillColor']);
+        
+        $this->gc->setFillColor('#333333');
+        $this->gc->commit();
+            
+        $state = $this->readCurrentGsState();
+        $this->assertEquals('#333333', $state['fillColor']);
+        
+        $this->gc->restoreGS();
+        $this->gc->commit();
+        
+        $state = $this->readCurrentGsState();
+        $this->assertEquals(null, $state['fillColor']);
+    }
+    
+    private function readCurrentGsState()
+    {
+        return $this->readAttribute($this->gc, 'state');
+    }
+    
+    /**
+     * @test
+     */
+    public function setAttributes()
+    {
+        $font = $this->getMock('PHPPdf\Core\Engine\Font');
+        
+        $attributes = array(
+            'fillColor' => '#222222',
+            'lineColor' => '#333333',
+            'lineWidth' => 3,
+            'lineDashingPattern' => array(array(1, 2, 0)),
+            'alpha' => 1,
+            'font' => array($font, 13),
+        );
+        
+        $expected = $attributes;
+        $expected['lineDashingPattern'] = $expected['lineDashingPattern'][0];
+        $expected['fontSize'] = $expected['font'][1];
+        $expected['font'] = $expected['font'][0];
+        
+        foreach($attributes as $name => $value)
+        {
+            call_user_func_array(array($this->gc, 'set'.$name), (array) $value);
+        }
+        
+        $this->gc->commit();
+        
+        $actual = $this->readCurrentGsState();
+        
+        ksort($expected);
+        ksort($actual);
+        
+        $this->assertEquals($expected, $this->readCurrentGsState());
+    }
+}
