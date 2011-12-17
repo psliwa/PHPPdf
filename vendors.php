3e269e0a1bb3a6bc003bc20dcc67c45d049d6475
psliwa <peter.pl7@gmail.com>
2011-12-17 20:42:57 +0100

    fix vendors.php

--- vendors.php
+++ vendors.php
@@ -10,8 +10,8 @@ function fetchGitVendors()
         array('Symfony/Component/Config', 'git://github.com/symfony/Config.git', 'master'),
         array('Markdown', 'git://github.com/wolfie/php-markdown.git', 'd464071334'),
         array('Zend/Pdf', 'git://github.com/psliwa/zend-pdf.git', 'master'),
-        array('Zend/Memory', 'git://github.com/knplabs/zend-memory.git', 'master'),
-        array('Zend/Cache', 'git://github.com/knplabs/zend-cache.git', 'master'),
+        array('Zend/Memory', 'git://github.com/KnpLabs/zend-memory.git', 'master'),
+        array('Zend/Cache', 'git://github.com/KnpLabs/zend-cache.git', 'master'),
         array('Imagine', 'git://github.com/avalanche123/Imagine.git', 'v0.2.6')
     );
     
