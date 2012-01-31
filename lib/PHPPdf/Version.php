72eaa0ad96ebfd9d544761f035971657605ea8e3
psliwa <peter.pl7@gmail.com>
2012-01-31 19:15:37 +0100

    merge master branch

--- lib/PHPPdf/Version.php
+++ lib/PHPPdf/Version.php
@@ -17,7 +17,7 @@ use PHPPdf\Exception\BadMethodCallException;
  */
 final class Version
 {
-    const VERSION = '1.1.2-DEV';
+    const VERSION = '1.1.1';
 
     private function __construct()
     {
