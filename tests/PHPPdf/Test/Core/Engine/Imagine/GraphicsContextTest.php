df9bf342e233324bc7f9ddb24942c3c304028590
Yordan Lazarov <sunrisestyle@abv.bg>
2014-09-08 02:04:56 +0300

    Update GraphicsContextTest.php

--- tests/PHPPdf/Test/Core/Engine/Imagine/GraphicsContextTest.php
+++ tests/PHPPdf/Test/Core/Engine/Imagine/GraphicsContextTest.php
@@ -7,7 +7,7 @@ use PHPPdf\Core\Engine\Imagine\Font;
 use PHPPdf\Core\Engine\EmptyImage;
 
 use PHPPdf\Core\Engine\Imagine\Image;
-use Imagine\Image\Color;
+use Imagine\Image\Palette\Color\RGB as Color;
 use PHPPdf\Bridge\Imagine\Image\Point;
 use Imagine\Image\Box;
 use PHPPdf\Core\Engine\Imagine\GraphicsContext;
