526b8303cd2a4f89db094c612ed3dfcbeeebc8d8
Yordan Lazarov <sunrisestyle@abv.bg>
2014-09-08 02:37:41 +0300

    Update GraphicsContext.php

--- lib/PHPPdf/Core/Engine/Imagine/GraphicsContext.php
+++ lib/PHPPdf/Core/Engine/Imagine/GraphicsContext.php
@@ -16,7 +16,9 @@ use PHPPdf\Core\Engine\AbstractGraphicsContext;
 use PHPPdf\Core\Engine\GraphicsContext as BaseGraphicsContext;
 use PHPPdf\Core\Engine\Image as BaseImage;
 use PHPPdf\Core\Engine\Font as BaseFont;
-use Imagine\Image\Palette\Color\RGB as ImagineColor;
+
+use Imagine\Image\Palette\RGB as ImagineColorPalette;
+
 use Imagine\Image\FontInterface as ImagineFont;
 use Zend\Barcode\Object\ObjectInterface as Barcode;
 
@@ -279,7 +281,8 @@ class GraphicsContext extends AbstractGraphicsContext
     {
         $alpha = (int) (100 - $this->state['alpha'] * 100);
         
-        return new ImagineColor($color, $alpha);
+        $palette = new ImagineColorPalette();
+        return $palette->color($color, $alpha);
     }
     
     protected function doSetFillColor($colorData)
