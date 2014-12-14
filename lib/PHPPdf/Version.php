75a81f28fac608fb658ee164fecd1264c31ed8cd
psliwa <peter.pl7@gmail.com>
2014-12-14 18:09:10 +0100

    revert PHPPdf\Version - it is however used

---
+++ lib/PHPPdf/Version.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * Copyright 2011 Piotr Śliwa <peter.pl7@gmail.com>
+ *
+ * License information is in LICENSE file
+ */
+
+namespace PHPPdf;
+
+use PHPPdf\Exception\BadMethodCallException;
+
+/**
+ * Current version of this library
+ *
+ * @author Piotr Śliwa <peter.pl7@gmail.com>
+ */
+final class Version
+{
+    const VERSION = '1.2.7-DEV';
+
+    private function __construct()
+    {
+        throw new BadMethodCallException(sprintf('Object of "%s" class can not be created.', __CLASS__));
+    }
+}
