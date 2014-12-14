22535e238954d9f90bd49d3cd00120da7ae57f79
psliwa <peter.pl7@gmail.com>
2014-12-14 18:09:52 +0100

    fix version

--- lib/PHPPdf/Version.php
+++ lib/PHPPdf/Version.php
@@ -17,7 +17,7 @@ use PHPPdf\Exception\BadMethodCallException;
  */
 final class Version
 {
-    const VERSION = '1.2.7-DEV';
+    const VERSION = '1.3.0-DEV';
 
     private function __construct()
     {
